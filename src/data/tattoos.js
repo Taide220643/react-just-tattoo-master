ce37ffc732a3aae9c8dfcd37413e1a8723c39849
Taide220643 <59428815+Taide220643@users.noreply.github.com>
2021-08-24 21:01:12 +0700

    Initial commit

---
+++ src/data/tattoos.js
@@ -0,0 +1,10 @@
+const tattoos = [
+    { title: "สูบซาเท่ห์ๆ", thumbnailUrl: "/images/tattoo-01-small.jpg", fullUrl: "/images/tattoo-01-large.jpg" },
+    { title: "ดื่มเหล้าเท่ห์ๆ", thumbnailUrl: "/images/tattoo-02-small.jpg", fullUrl: "/images/tattoo-02-large.jpg" },
+    { title: "นอนหลับเท่ห์ๆ", thumbnailUrl: "/images/tattoo-03-small.jpg", fullUrl: "/images/tattoo-03-large.jpg" },
+    { title: "หล่อสวยเท่ห์ๆ", thumbnailUrl: "/images/tattoo-04-small.jpg", fullUrl: "/images/tattoo-04-large.jpg" },
+    { title: "ดมกลิ่นตีนเท่ห์ๆ", thumbnailUrl: "/images/tattoo-05-small.jpg", fullUrl: "/images/tattoo-05-large.jpg" },
+    { title: "ตัดผมเท่ห์ๆ", thumbnailUrl: "/images/tattoo-06-small.jpg", fullUrl: "/images/tattoo-06-large.jpg" },
+];
+
+export default tattoos;
