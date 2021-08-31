dd929daad2113bd5025d3e269de53cf78944dbdb
Taide220643 <59428815+Taide220643@users.noreply.github.com>
2021-08-31 16:55:26 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,43 @@
 import { useState } from 'react';
 import './App.css';
 import AppHeader from './components/AppHeader';
-import AppSearch from './components/AppSearch';
 import TattooItem from './components/TattooItem';
-import TattooPost from './components/TattooPost';
-import tattoos from './data/tattoos';
+import { map } from 'async';
 
 function App() {
-    const [selectedTattoo, setSelectedTattoo] = useState(null);
-    const [searchText, setSearchText] = useState('');
-
-    function onTattooOpenClick(tattoo) {
-        setSelectedTattoo(tattoo);
-    }
-
-    function onTattooCloseClick() {
-        setSelectedTattoo(null);
-    }
-
-    const tattooItems = tattoos.filter((tattoo) => {
-        return tattoo.title.includes(searchText);
-    }).map((tattoo, index) => {
-        return <TattooItem key={index} tattoo={tattoo} onTattooClick={onTattooOpenClick} />;
-    });
-
-    let tattooPost = null;
-    if (!!selectedTattoo) {
-        tattooPost = <TattooPost tattoo={selectedTattoo} onBgClick={onTattooCloseClick} />;
-    }
 
     return (
         <div className="app">
             <AppHeader />
-            <section className="app-section">
-                <div className="app-container">
-                    <AppSearch value={searchText} onValueChange={setSearchText} />
-                    <div className="app-grid">
-                        {tattooItems}
-                    </div>
-                </div>
-            </section>
-            {tattooPost}
-        </div>
+            {/* <TattooItem /> */}
+          
+            <img class="w3-image" src="/images/architect.jpg" alt="Architecture" width="1500" height="800"></img>
+
+            <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-black w3-opacity-min">
+        <b>BR</b></span> <span class="w3-hide-small w3-text-light-grey">Architects</span></h1>
+  </div>
+
+  <div class="w3-container w3-padding-32" id="projects"></div>
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Projects</h3>
+    <h4 class="w3-col l3 m6 w3-margin-bottom"></h4>
+
+
+
+   
+ 
+    <div class="w3-container w3-padding-32" id="about"> </div>
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">About</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat.
+    </p>
+
+   
+
+</div>
+
+
     );
 }
 
---
+++ src/components/map.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import AppHeader from './components/AppHeader';
+import TattooItem from './components/TattooItem';
+import { map } from 'async';
+
+function App() {
+
+    return (
+ 
+<button class="w3-button w3-black w3-section" type="submit">
+        <i class="fa fa-paper-plane"></i> SEND MESSAGE
+      </button>
+
+
+
+    );
+}
